b82e72ff7619993b40a6e44c67e3379072d8580a
Nata Oliveira <nata.oliveira@mv.com.br>
2023-11-21 08:26:53 -0300

    criando a tabela

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css';
 import Cabecalho from './components/Cabecalho';
 import Container from './components/Container';
 import Rodape from './components/Rodape';
+import Tabela from './components/Tabela';
 import Titulo from './components/Título';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Cabecalho />
       <Container>
         <Titulo>Área Administrativa</Titulo>
+        <Tabela />
       </Container>
       <Rodape />
     </>
